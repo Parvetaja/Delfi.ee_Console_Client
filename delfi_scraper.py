e74b8f682407a79a4ff63ce5b640470b38370e49
jparve <jparve@ttu.ee>
2019-01-03 22:39:10 +0200

    Content is now adaptive to the width of the console window. Also bug fixes.

--- delfi_scraper.py
+++ delfi_scraper.py
@@ -13,19 +13,23 @@ class Scraper:
     def __init__(self):
         self.headline_content = []
         self.pages = {"avaleht": "http://www.delfi.ee/", "sport": "http://www.delfi.ee/sport",
-                      "forte":"http://www.forte.delfi.ee/", "arileht":"http://www.arileht.delfi.ee/"}
-        print("""            _____  ______ _      ______ _____ 
+                      "forte": "http://www.forte.delfi.ee/", "arileht": "http://www.arileht.delfi.ee/"}
+        print("""             _____  ______ _      ______ _____ 
             |  __ \|  ____| |    |  ____|_   _|
             | |  | | |__  | |    | |__    | |  
             | |  | |  __| | |    |  __|   | |  
             | |__| | |____| |____| |     _| |_ 
             |_____/|______|______|_|    |_____|                                                    
+            DELFI.EE terminalibrauser
         """)
-        print("DELFI.EE terminalibrauser")
         self.user_action()
+
+    def get_window_width(self):
+        return os.get_terminal_size().columns
 
     def user_action(self):
         while True:
+            print("\n" + self.get_window_width() * "_")
             action = input("Delfi uudised: [avaleht, sport, forte, arileht]\n"
                            "Stop: [q],\n"
                            "Artikkel: [number]\n")
@@ -38,7 +42,7 @@ class Scraper:
                 continue
             try:
                 os.system("cls")
-                if len(self.headline_content) != 0:
+                if len(self.headline_content) != 0 and 0 < int(action) < len(self.headline_content):
                     print(self.article(int(action)))
             except ValueError:
                 continue
@@ -55,23 +59,27 @@ class Scraper:
 
                 if new not in self.headline_content:
                     self.headline_content.append(new)
-                    result += f"{headlines.index(news)+1}. {textwrap.fill(news.a.string, width=90)}" + "\n"
+                    number = headlines.index(news) + 1
+                    result += f"{' ' * (3-len(str(number)))}{number}. {textwrap.fill(news.a.string, width=self.get_window_width())}" + "\n"
 
         return result
 
     def article(self, index_of_article):
-        url = self.headline_content[index_of_article-1]
+        url = self.headline_content[index_of_article - 1]
 
         with urlopen(url[1]) as page:
             html = bs(page, "html.parser")
-            lead = str(html.find_all("p", {"class": "article__chunk article__chunk--lead"})[0]).split(">")[1].strip("</p>")
+            lead = str(html.find_all("p", {"class": "article__chunk article__chunk--lead"})[0]).split(">")[1].strip(
+                "</p>")
             body = html.find_all("div", {"class": "article__body"})[0].find_all("p")
 
             final_body = ""
             for paragraph in body:
-                final_body += re.sub(r"<\/?\w+>", "", textwrap.fill(str(paragraph), width=120) + "\n" + "\n")
+                final_body += re.sub(r"<\/?\w+>", "",
+                                     textwrap.fill(str(paragraph), width=self.get_window_width()) + "\n" + "\n")
 
             return str(url[0]) + "\n" + "\n" + lead.strip() + "\n" + "\n" + final_body + url[1]
 
 
-scrap = Scraper()
+scraper = Scraper()
+scraper.get_window_width()
